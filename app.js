ca6fa20cb4f9fa81eaf76aae9729613d19f6db10
Jessica Nascimento <jessicanascimento07001@gmail.com>
2024-06-20 12:57:24 -0300

    Atualiza arquivos para versão final

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let tentativas = 1;
 function exibirTextoTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 
 function exibirMensagemInicial(){
@@ -48,7 +49,6 @@ function gerarNumeroAleatorio(){
         return gerarNumeroAleatorio();
     }else{
         ListaNumerosSorteados.push(numeroEscolhido);
-        console.log(ListaNumerosSorteados);
         return numeroEscolhido;
     }
 }
