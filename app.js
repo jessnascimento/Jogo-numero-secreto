14ca78b022daec4035e9f2b8322cc1e0f1bbc9a5
Jessica Nascimento <jessicanascimento07001@gmail.com>
2024-06-20 11:59:11 -0300

    Atualiza arquivos para segunda versao do codigo

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
-alert('Boas vindas ao jogo do número secreto');
-let numeroMax = 5000;
-let numeroSecreto = parseInt(Math.random() * numeroMax + 1);
-let chute;
-let tentativas = 1;
+let numeroSecreto = gerarNumeroAleatorio();
 
-while(chute != numeroSecreto){
-    chute = prompt(`Escolha um numero entre 1 e ${numeroMax}`);
-    if(chute == numeroSecreto){
-        break;
-    } else{
-        if(numeroSecreto > chute){
-            alert(`O numero secreto é maior que o ${chute}`);
-        }else{
-            alert(`O numero secreto é menor que o ${chute}`);
-        }
-        tentativas++;
-    }
+function exibirTextoTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
 }
 
-let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-console.log(`Você descobriu o numero secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}!`);
+exibirTextoTela('h1', 'Jogo do número secreto');
+exibirTextoTela('p', 'Escolha um número entre 1 e 100');
+
+function verificarChute(){
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
+}
+
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * 10 + 1);
+}
