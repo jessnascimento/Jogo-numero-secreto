0747f156d2c1f0909591d10c226ac07cfac7479b
jessnascimento <jessicanascimento07001@gmail.com>
2024-06-20 12:14:42 -0300

    Adiciona arquivos de terceira versao do codigo

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoTela(tag, texto){
     let campo = document.querySelector(tag);
@@ -6,11 +7,25 @@ function exibirTextoTela(tag, texto){
 }
 
 exibirTextoTela('h1', 'Jogo do número secreto');
-exibirTextoTela('p', 'Escolha um número entre 1 e 100');
+exibirTextoTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute(){
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
+    if (chute == numeroSecreto){
+        exibirTextoTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o numero secreto com ${tentativas} ${palavraTentativa}!`
+        exibirTextoTela('p', mensagemTentativas);
+    }else{
+        if (chute > numeroSecreto){
+            exibirTextoTela('h1', 'Tente novamente!');
+            exibirTextoTela('p', 'Chute maior que numero secreto.');
+        }else{
+            exibirTextoTela('h1', 'Tente novamente!');
+            exibirTextoTela('p', 'Chute menor que numero secreto.');
+        }
+        tentativas++
+    }
 }
 
 function gerarNumeroAleatorio(){
